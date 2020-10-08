e3b875e2e53af69b88829c660304b917f5ab4539
drewthedev9@gmail.com <45505005+drewthedev9-tech@users.noreply.github.com>
2020-10-08 01:08:57 -0400

    added react-redux-demo

---
+++ react-redux-demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CakeContainer from './components/cakeContainer.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <CakeContainer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux-demo/src/components/cakeContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const cakeContainer = () => {
+    return (
+        <div>
+        <h2>Number of cakes</h2>
+        <button>Buy cakes</button>
+        </div>
+      );
+}
+ 
+export default cakeContainer;
